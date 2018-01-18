7430acdb5b49491e8bf86708b68d4591ed2d2d5f
Justin Lardani and Manuela Garcia <justin@>
2018-01-18 15:09:19 -0800

    add scripting to finish pig latin function.

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,39 @@ $(document).ready(function() {
     var sentence = $("#sentence").val();
     var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
     var pigLatin = '';
-      vowels.forEach(function(vowel) {
+    var isVowel = false;
+    var numCons = 1;
+    var isCons = true;
+
+    vowels.forEach(function(vowel) {
         if (sentence[0] === vowel) {
           pigLatin = sentence + "way";
+          isVowel = true;
       };
       console.log(pigLatin);
     });
 
+    if (isVowel === false) {
+      for (var i = 1; isCons === true; i++) {
+        vowels.forEach(function(vowel) {
+          if (sentence[i] === vowel) {
+            isCons = false;
+          } //else
+        });
+        if (isCons === true) {
+          numCons = numCons + 1;
+        };
+      };
+        console.log(isCons);
+        console.log(numCons);
+
+    };
+    if (isVowel === false) {
+      var front = sentence.slice(0, numCons);
+      var back = sentence.slice(numCons, sentence.length);
+      var newWord = back + front + "ay";
+      $("#response").text("Your word translated into pig lat in is: " + newWord + ".");
+    };
+    
   });
-
-
-
 });
