9a239bd275a8d05261e278635717d8484cf35f32
Justin Lardani and Manuela Garcia <manuela@>
2018-01-18 15:12:58 -0800

    add comments to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
     event.preventDefault();
 
+    //initializes variables
     var sentence = $("#sentence").val();
     var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
     var pigLatin = '';
@@ -11,6 +12,7 @@ $(document).ready(function() {
     var numCons = 1;
     var isCons = true;
 
+    //determine if word starts with vowel. If it does, return Pig Latin
     vowels.forEach(function(vowel) {
         if (sentence[0] === vowel) {
           pigLatin = sentence + "way";
@@ -19,6 +21,7 @@ $(document).ready(function() {
       console.log(pigLatin);
     });
 
+    //if word does not start with vowel, count consonants up to first vowel
     if (isVowel === false) {
       for (var i = 1; isCons === true; i++) {
         vowels.forEach(function(vowel) {
@@ -34,12 +37,15 @@ $(document).ready(function() {
         console.log(numCons);
 
     };
+
+    //Split word into two parts (first part up to first vowel, second part is rest of word)
+    //then create new Pig Latin word (back of word + front of word + ay)
     if (isVowel === false) {
       var front = sentence.slice(0, numCons);
       var back = sentence.slice(numCons, sentence.length);
       var newWord = back + front + "ay";
       $("#response").text("Your word translated into pig lat in is: " + newWord + ".");
     };
-    
+
   });
 });
